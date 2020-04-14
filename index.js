05ea43346a60902b927ae90b837e01268f4c24a0
Mads Dupont Heidemann <mdhe@nuuday.dk>
2020-04-14 14:53:10 +0200

    First

---
+++ index.js
@@ -0,0 +1,16 @@
+var jsforce = require('jsforce');
+var conn = new jsforce.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  loginUrl : 'https://test.salesforce.com'
+});
+conn.login('mdhe@tdc.dk.erhverv.fulfilment', 'Podio123!', function(err, userInfo) {
+  if (err) { return console.error(err); }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log(conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+  // ...
+});
